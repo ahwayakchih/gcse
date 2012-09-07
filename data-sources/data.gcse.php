3c4d8face153f7bebdb4647c77bdc5bd0b5a661f
Brian Zerangue <brian.zerangue@gmail.com>
2012-09-07 09:58:39 -0500

    updated date in and version number in data.gcse.php

--- data-sources/data.gcse.php
+++ data-sources/data.gcse.php
@@ -59,8 +59,8 @@
 				"author" => array("name" => "Marcin Konicki",
 					"website" => "http://ahwayakchih.neoni.net",
 					"email" => "ahwayakchih@neoni.net"),
-				"version" => "2.2",
-				"release-date" => "2008-12-18",
+				"version" => "2.3",
+				"release-date" => "2009-06-25",
 				"recognised-url-param" => $params,
 			);
 		}
